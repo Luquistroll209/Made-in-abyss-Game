c441960ae552af3b23daf625324f3ae4c4df76b4
Luquistroll209 <luquistroll@gmail.com>
2025-01-31 09:15:32 +0100

    TweenSolved

--- scripts/Curse.gd
+++ scripts/Curse.gd
@@ -16,38 +16,38 @@ func _process(delta):
 	if is_multiplayer_authority():
 		var heightNumber = int(Player.global_position.y)
 		heightText.text = str(heightNumber) + "M"
-		var tween = get_tree().create_tween()
-		# Capa 1: de 0 a 1350 metros (umbral 50 metros)
+		
+		# Layer 1: from 0 to 1350 meters (threshold 50 meters)
 		if heightNumber > 0:
-			tween.tween_property(CurseIMG, "modulate:a", 0, 2.0)
+			var tween = get_tree().create_tween()
+			tween.tween_property(CurseIMG, "modulate:a", 0, 2.0)  # Add Tweener here
 		elif heightNumber <= 0 and heightNumber >= -1350:
-			CurseIMG.modulate.a = (heightNumber - last_height) / 17 #Change transparency of the node CurseIMG
+			CurseIMG.modulate.a = (heightNumber - last_height) / 17 # Change transparency of the CurseIMG node
 			if heightNumber - last_height >= 30:
 				vomit()
-				Player.Hunger -= 10  #Efect of de curse of abyss
-				last_height = heightNumber  # Update de last_height to actual last height
-			
+				Player.Hunger -= 10  # Effect of the abyss curse
+				last_height = heightNumber  # Update last_height to the current height
 
-		# Capa 2: de 1351 a 2500 metros (umbral 30 metros)
+		# Layer 2: from 1351 to 2500 meters (threshold 30 meters)
 		elif heightNumber >= 1351 and heightNumber <= 2500:
 			if Player.global_position.y - 30 >= 1351:
 				pass
 
-		# Capa 3: de 2501 a 7000 metros (umbral 20 metros)
+		# Layer 3: from 2501 to 7000 meters (threshold 20 meters)
 		elif heightNumber >= 2501 and heightNumber <= 7000:
 			if Player.global_position.y - 20 >= 2501:
 				pass
 
-		# Capa 4: de 7001 a 10000 metros (umbral 10 metros)
+		# Layer 4: from 7001 to 10000 meters (threshold 10 meters)
 		elif heightNumber >= 7001 and heightNumber <= 10000:
 			if Player.global_position.y - 10 >= 7001:
 				pass
 				
 		if Player.global_position.y < last_height:
-			last_height = Player.global_position.y # Actualizamos solo si el jugador está bajando
+			last_height = Player.global_position.y # Update only if the player is descending
 			#print(last_height)
 			
 func vomit():
-		VomitParticle.visible = true
-		await get_tree().create_timer(5.0).timeout
-		VomitParticle.visible = false
+	VomitParticle.visible = true
+	await get_tree().create_timer(5.0).timeout
+	VomitParticle.visible = false
